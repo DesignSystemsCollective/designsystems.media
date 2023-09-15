760b31df7ff68e5d1ef97b00ef418a5eef12cd9e
Josh Harwood <josh@noworries.nz>
2023-09-15 16:13:20 +1200

    fixed images

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,16 +2,17 @@ import { defineCollection, z } from "astro:content";
 
 const mediaCollection = defineCollection({
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    // Transform string to Date object
-    publishedAt: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    image: z.string().optional(),
-    poster: z.string().optional(),
-    videoUrl: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      // Transform string to Date object
+      publishedAt: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      image: image(),
+      poster: image(),
+      videoUrl: z.string().optional(),
+    }),
 });
 export const collections = {
   media: mediaCollection,
