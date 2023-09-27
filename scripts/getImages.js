6b9552404612c8a3934c0dea8366aed10807fa51
Josh Harwood <josh@noworries.nz>
2023-09-27 17:45:38 +1300

    added new local images

--- scripts/getImages.js
+++ scripts/getImages.js
@@ -23,30 +23,80 @@ function processMarkdownFile(filePath) {
   console.log(`Processing: ${filePath}`);
 
   const markdownContent = fs.readFileSync(filePath, "utf8");
-  const { data } = matter(markdownContent);
+  const { data, content } = matter(markdownContent);
 
-  if (data.image) {
+  if (data.image && data.localImages === false) {
     const imageFileName = path.basename(data.image);
     const imageOutputPath = path.join(path.dirname(filePath), imageFileName);
 
     console.log(`Downloading: ${data.image}`);
     downloadImage(data.image, imageOutputPath)
-      .then(() => console.log(`Downloaded: ${data.image}`))
+      .then(() => {
+        console.log(`Downloaded: ${data.image}`);
+        // Change localImages to true after processing
+        data.localImages = true;
+        // Update the "image" key with the local file path wrapped in double quotes
+        data.image = `./${imageFileName}`;
+
+        // Create the updated front matter string
+        const updatedFrontMatter = Object.entries(data)
+          .map(([key, value]) => `${key}: ${JSON.stringify(value)}`)
+          .join("\n");
+
+        // Combine the updated front matter and content
+        const updatedMarkdown = `---\n${updatedFrontMatter}\n---\n${content}`;
+
+        // Update the content of the file
+        fs.writeFileSync(filePath, updatedMarkdown);
+      })
       .catch((err) =>
         console.error(`Error downloading ${data.image}: ${err.message}`)
       );
   }
 
-  if (data.poster) {
+  if (data.poster && data.localImages === false) {
     const posterFileName = path.basename(data.poster);
     const posterOutputPath = path.join(path.dirname(filePath), posterFileName);
 
     console.log(`Downloading: ${data.poster}`);
     downloadImage(data.poster, posterOutputPath)
-      .then(() => console.log(`Downloaded: ${data.poster}`))
-      .catch((err) =>
-        console.error(`Error downloading ${data.poster}: ${err.message}`)
-      );
+      .then(() => {
+        console.log(`Downloaded: ${data.poster}`);
+        // Change localImages to true after processing
+        data.localImages = true;
+        // Update the "poster" key with the local file path wrapped in double quotes
+        data.poster = `./${posterFileName}`;
+
+        // Create the updated front matter string
+        const updatedFrontMatter = Object.entries(data)
+          .map(([key, value]) => `${key}: ${JSON.stringify(value)}`)
+          .join("\n");
+
+        // Combine the updated front matter and content
+        const updatedMarkdown = `---\n${updatedFrontMatter}\n---\n${content}`;
+
+        // Update the content of the file
+        fs.writeFileSync(filePath, updatedMarkdown);
+      })
+      .catch((err) => {
+        console.error(`Error downloading ${data.poster}: ${err.message}`);
+
+        // Change localImages to true after processing
+        data.localImages = true;
+        // Update the "poster" key with the local file path wrapped in double quotes
+        data.poster = `./hqdefault.jpg`;
+
+        // Create the updated front matter string
+        const updatedFrontMatter = Object.entries(data)
+          .map(([key, value]) => `${key}: ${JSON.stringify(value)}`)
+          .join("\n");
+
+        // Combine the updated front matter and content
+        const updatedMarkdown = `---\n${updatedFrontMatter}\n---\n${content}`;
+
+        // Update the content of the file
+        fs.writeFileSync(filePath, updatedMarkdown);
+      });
   }
 }
 
