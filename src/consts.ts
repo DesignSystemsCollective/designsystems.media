de98bf06795af85525f6a55091c40e399bdd026a
Josh Harwood <josh@noworries.nz>
2023-10-11 09:07:12 +1300

    tweak title

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,4 @@
 
 export const SITE_TITLE = "DesignSystems.media";
 export const SITE_DESCRIPTION =
-  "Design systems video library, aggregating amazing content from various video platforms";
+  "Design systems media library, aggregating amazing content from various media platforms";
