06de4c25c8367837d42c6ecf61ec553b19d3d63e
Josh Harwood <josh@noworries.nz>
2023-10-12 23:25:28 +1300

    New source, lots of drafts

--- scripts/ignoreVideos.js
+++ scripts/ignoreVideos.js
@@ -25,5 +25,7 @@ module.exports = [
   "https://www.youtube.com/watch?v=mEtytXNYTaI",
   "https://www.youtube.com/watch?v=wvrnzn3zBYQ",
   "https://www.youtube.com/watch?v=HcxckpdOVpM",
+  "https://www.youtube.com/watch?v=Hn0Ow105w58",
+  "https://www.youtube.com/watch?v=iWyoQb4UPnU",
   // Add more video URLs to ignore as needed
 ];
