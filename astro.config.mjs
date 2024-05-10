9e28834ea1259aad337690efb6ac2fa7613ab0a1
Simon Vrachliotis <simon@simonswiss.com>
2024-05-10 13:26:24 +1000

    Keystatic setup + WIP media collection schema

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import pagefind from "astro-pagefind";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import pagefind from 'astro-pagefind'
+import sitemap from '@astrojs/sitemap'
 
-import sitemap from "@astrojs/sitemap";
+import react from '@astrojs/react'
+import keystaticAstro from '@keystatic/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://designsystems.media",
-  integrations: [mdx(), sitemap(), pagefind()],
-});
+  site: 'https://designsystems.media',
+  integrations: [mdx(), sitemap(), pagefind(), react(), keystaticAstro()],
+  output: 'hybrid',
+})
