b625ef5c35d7fa08381a33914db5fcdae3cc0e73
Simon Vrachliotis <simon@simonswiss.com>
2024-05-10 15:30:11 +1000

    schema continued

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -1,48 +1,55 @@
-import { collection, config, fields } from '@keystatic/core'
+import { collection, config, fields } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: "local",
   },
   ui: {
     brand: {
-      name: 'DesignSystems.media',
+      name: "DesignSystems.media",
     },
   },
   collections: {
     media: collection({
-      label: 'Media',
-      path: 'src/content/media/**',
-      slugField: 'title',
+      label: "Media",
+      path: "src/content/media/*/",
+      slugField: "title",
       format: {
-        contentField: 'content',
+        contentField: "content",
       },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
-        description: fields.text({ label: 'Description', multiline: true }),
-        publishedAt: fields.date({ label: 'Published At', validation: { isRequired: true } }),
-        dateAdded: fields.date({ label: 'Date added' }),
-        image: fields.image({ label: 'Image' }),
-        poster: fields.image({ label: 'Poster' }),
-        duration: fields.text({ label: 'Duration' }),
-        privacyStatus: fields.text({ label: 'Privacy Status' }),
-        videoUrl: fields.text({ label: 'Video URL' }),
-        tags: fields.array(fields.text({ label: 'Tag' }), {
-          label: 'Tags',
+        title: fields.slug({ name: { label: "Title" } }),
+        description: fields.text({ label: "Description", multiline: true }),
+        publishedAt: fields.date({
+          label: "Published At",
+          validation: { isRequired: true },
+        }),
+        dateAdded: fields.date({ label: "Date added" }),
+        image: fields.text({ label: "Image" }),
+        poster: fields.text({ label: "Poster" }),
+        localImages: fields.checkbox({
+          label: "Local Images",
+          defaultValue: true,
+        }),
+        duration: fields.text({ label: "Duration" }),
+        privacyStatus: fields.text({ label: "Privacy Status" }),
+        videoUrl: fields.text({ label: "Video URL" }),
+        tags: fields.array(fields.text({ label: "Tag" }), {
+          label: "Tags",
           itemLabel: (data) => data.value,
         }),
-        categories: fields.array(fields.text({ label: 'Category' }), {
-          label: 'Categories',
+        categories: fields.array(fields.text({ label: "Category" }), {
+          label: "Categories",
           itemLabel: (data) => data.value,
         }),
-        speakers: fields.array(fields.text({ label: 'Speaker' }), {
-          label: 'Speakers',
+        speakers: fields.array(fields.text({ label: "Speaker" }), {
+          label: "Speakers",
           itemLabel: (data) => data.value,
         }),
-        draft: fields.checkbox({ label: 'Draft' }),
+        draft: fields.checkbox({ label: "Draft" }),
         // Longform copy
-        content: fields.mdx({ label: 'Content' }),
+        content: fields.mdx({ label: "Content" }),
       },
     }),
   },
-})
+});
