3b9e1ee1132d6c45b36611aa596b9a1a87d8ee51
Frank Congson <fcongson@gmail.com>
2024-05-11 10:16:17 +1200

    Setup github mode for keystatic

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -2,7 +2,9 @@ import { collection, config, fields } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
+    kind: "github",
+    repo: "DesignSystemsCollective/designsystems.media",
+    branchPrefix: "keystatic/",
   },
   ui: {
     brand: {
