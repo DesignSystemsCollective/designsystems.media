4bbd900ea41006ca5644a2fb6cbcf32244524a7a
Josh Harwood <josh@noworries.nz>
2025-05-13 23:50:25 +1200

    generate sitemap as page

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,50 +9,16 @@ import keystaticAstro from "@keystatic/astro";
 
 import netlify from "@astrojs/netlify";
 
-const sitemapConfig = {
-  // print date not time
-  lastmodDateOnly: true,
-
-  // set the xml namespace
-  xmlns: {
-    xhtml: true,
-    news: false,
-    image: false,
-    video: true,
-  },
-
-  serialize(item) {
-    if (/video/.test(item.url) && item.data?.video) {
-      const videoData = item.data.video;
-
-      item.changefreq = EnumChangefreq.DAILY;
-      item.lastmod = new Date().toISOString();
-      item.priority = 0.9;
-
-      item.video = [
-        {
-          thumbnail_loc: videoData.thumbnail,
-          title: videoData.title,
-          description: videoData.description,
-          player_loc: videoData.player,
-          "player_loc:autoplay": videoData.autoplay ? "ap=1" : undefined,
-          "player_loc:allow_embed": videoData.allowEmbed ? "yes" : undefined,
-        },
-      ].filter(Boolean); // remove undefined
-    }
-
-    return item;
-  },
-
-  createLinkInHead: true,
-};
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://designsystems.media",
   integrations: [
     mdx(),
-    sitemap(sitemapConfig),
+    sitemap({
+      filter(page) {
+        return !/video/.test(page);
+      },
+    }),
     pagefind(),
     react(),
     keystaticAstro(),
