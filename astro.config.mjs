1ffa7638491749532fc239d6d83c28ad5743dbb4
Frank Congson <fcongson@gmail.com>
2025-05-15 15:37:20 +1200

    Update trailing slashes for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 import pagefind from "astro-pagefind";
 import { defineConfig, passthroughImageService } from "astro/config";
-import icon from "astro-icon";
 
 import react from "@astrojs/react";
 import keystaticAstro from "@keystatic/astro";
@@ -12,10 +12,18 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   site: "https://designsystems.media",
-  integrations: [mdx(), sitemap(), pagefind(), react(), keystaticAstro(), icon()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    pagefind(),
+    react(),
+    keystaticAstro(),
+    icon(),
+  ],
   output: "static",
   adapter: netlify(),
   image: {
     service: passthroughImageService(),
   },
+  trailingSlash: "always",
 });
