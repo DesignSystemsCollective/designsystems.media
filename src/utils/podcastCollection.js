5561eef7cdae9c156b7d2555104614892f96aa00
Josh Harwood <josh@noworries.nz>
2025-06-04 23:00:21 +1200

    Astro setup

---
+++ src/utils/podcastCollection.js
@@ -0,0 +1,28 @@
+import { getCollection } from "astro:content";
+import { isDurationOneMinuteOrUnder } from "./isDurationOneMinuteOrUnder";
+
+export const allPosts = await getCollection("podcast");
+
+export const allPostsFilteredAndSorted = allPosts
+  .filter((post) => {
+    return !post.data.draft && !isDurationOneMinuteOrUnder(post.data.duration);
+  })
+  .sort((a, b) => b.data.publishedAt.valueOf() - a.data.publishedAt.valueOf());
+
+export const postCount = allPostsFilteredAndSorted.length;
+
+export const postsWithTags = allPostsFilteredAndSorted.filter((post) => {
+  return post.data.tags && post.data.tags.length > 0;
+});
+
+export const tags = [
+  ...new Set(postsWithTags.flatMap((post) => post.data.tags)),
+].sort();
+
+export const postsWithSpeakers = allPostsFilteredAndSorted.filter((post) => {
+  return post.data.speakers && post.data.speakers.length > 0;
+});
+
+export const speakers = [
+  ...new Set(postsWithSpeakers.flatMap((post) => post.data.speakers)),
+].sort();
